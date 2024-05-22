733fa5e055a1573dae8489e6be03fae859a598bf
Sarit Ritwirune <sarit@elcolie.com>
2024-05-22 16:13:19 +0700

    Add description, version, terms_of_service, contact, license_info, openapi_tags, openapi_url.

--- main.py
+++ main.py
@@ -29,7 +29,45 @@ logging.basicConfig(
 # Create a logger object
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+description = """
+**CRUD Taskado** todo Task API with undo feature.
+
+- You need to create an user before creating a task.
+- You can create, update, delete, list and undo a task.
+- You can `undo` the last `UPDATE`, `DELETE` to the task.
+- You can filter the tasks by due_date, task_status, created_by_username, updated_by_username.
+
+"""
+
+tags_metadata = [
+    dict(
+        name="tasks",
+        description="CRUD operations",
+    ),
+    dict(
+        name="undo",
+        description="Undo the last UPDATE, DELETE to the task",
+    )
+]
+
+
+app = FastAPI(
+    title="CRUD Taskado todo Task API",
+    description=description,
+    version='0.0.1',
+    terms_of_service="https://creativecommons.org/terms/",
+    contact=dict(
+        name="Sarit",
+        url="https://github.com/elcolie",
+        email="cs.sarit@gmail.com"
+    ),
+    license_info=dict(
+        name="Apache 2.0",
+        url="https://www.apache.org/licenses/LICENSE-2.0.html"
+    ),
+    openapi_tags=tags_metadata,
+    openapi_url="/api/v1/openapi.json",
+)
 
 
 class Tags(Enum):
@@ -38,10 +76,10 @@ class Tags(Enum):
 
 
 @app.post('/create-task/',
-          description="Create todo task",
+          summary="Create todo task",
           status_code=status.HTTP_201_CREATED,
           response_model=TaskSuccessMessage,
-          tags=[Tags.tasks.value]
+          tags=[Tags.tasks]
           )
 async def _create_task(
     task_input: typ.Annotated[
@@ -84,38 +122,78 @@ async def _create_task(
             }
         )
     ]) -> typ.Any:
-    """Endpoint to create a task."""
+    """
+    Endpoint to create a task.
+
+    - **title**: The title of the task.
+    - **description**: The description of the task.
+    - **status**: The status of the task. It must be either 'pending', 'in_progress' or 'done'.
+    - **due_date**: The due date of the task in 'YYYY-MM-DD' format.
+    - **created_by**: The user id who created the task.
+    """
     return create_task(task_input)
 
 
-@app.delete('/{task_id}', description="Delete task", status_code=status.HTTP_204_NO_CONTENT, tags=[Tags.tasks.value])
+@app.delete('/{task_id}',
+            summary="Delete todo task",
+            status_code=status.HTTP_204_NO_CONTENT, tags=[Tags.tasks])
 async def _delete_task(task_id: CurrentTaskContent = Depends(valid_task)) -> None:
-    """Endpoint to delete a task."""
+    """
+    Endpoint to delete a task.
+
+    - **task_id**: The id of the task to delete.
+    """
     delete_task(task_id)
     return
 
 
-@app.get('/{task_id}', description="Get task detail", response_model=UpdateTask, tags=[Tags.tasks.value])
+@app.get('/{task_id}',
+         summary="Get task detail",
+         response_model=UpdateTask, tags=[Tags.tasks])
 async def _get_task(task_id: CurrentTaskContent = Depends(valid_task)) -> typ.Any:
+    """
+    Endpoint to get a task detail.
+
+    - **task_id**: The id of the task to get.
+    """
     return get_task(task_id)
 
 
-@app.get('/', description="List out tasks", response_model=Page[SummaryTask], tags=[Tags.tasks.value])
+@app.get('/',
+         summary="List tasks",
+         response_model=Page[SummaryTask], tags=[Tags.tasks])
 async def _list_tasks(
     commons: typ.Annotated[
         ConcreteCommonTaskQueryParams,
         Depends(validate_task_common_query_param)
     ],
 ) -> typ.Any:
+    """
+    Endpoint to list all tasks.
+
+    - **due_date**: The due date of the task in 'YYYY-MM-DD' format.
+    - **task_status**: The status of the task. It must be either 'pending', 'in_progress' or 'done'.
+    - **created_by_username**: The username of the user who created the task.
+    - **updated_by_username**: The username of the user who updated the task.
+    """
     return paginate(list_tasks(commons))
 
 
-@app.post('/undo/{task_id}', description="Undo task", response_model=TaskSuccessMessage, tags=[Tags.undo.value])
+@app.post('/undo/{task_id}',
+          summary="Undo task",
+          response_model=TaskSuccessMessage, tags=[Tags.undo])
 async def _undo_task(task_id: CheckTaskId = Depends(valid_undo_task)) -> typ.Any:
+    """
+    description="Undo last UPDATE, DELETE to the task",
+
+    - **task_id**: The id of the task to undo.
+    """
     return undo_task(task_id)
 
 
-@app.put('/', description="Update task", response_model=TaskSuccessMessage, tags=[Tags.tasks.value])
+@app.put('/',
+         summary="Update task",
+         description="Make another revision of the task", response_model=TaskSuccessMessage, tags=[Tags.tasks])
 async def _update_task(
     payload: typ.Annotated[
         UpdateTask,
